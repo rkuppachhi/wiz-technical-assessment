14670a7d85abb147bdff72524171681b64b1b4d1
Rahul  Kuppachhi <rahulkuppachhi@Rahuls-MacBook-Pro.local>
2026-04-19 12:48:13 -0400

    fix error

--- main.tf
+++ main.tf
@@ -11,8 +11,10 @@ resource "aws_vpc" "wiz_vpc" {
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.wiz_vpc.id
   cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true # Required for SSH access
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a" # THIS LINE FIXES THE ERROR
 }
+
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.wiz_vpc.id
