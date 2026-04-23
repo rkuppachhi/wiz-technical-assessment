840f944bd1b3289a6450aa2cf99c85df88b145e9
Rahul  Kuppachhi <rahulkuppachhi@Rahuls-MacBook-Pro.local>
2026-04-23 13:11:58 -0400

    update

--- main.tf
+++ main.tf
@@ -279,6 +279,9 @@ resource "aws_securityhub_account" "main" {}
 resource "aws_securityhub_standards_subscription" "aws_best_practices" {
   depends_on    = [aws_securityhub_account.main]
   standards_arn = "arn:aws:securityhub:us-east-1::standards/aws-foundational-security-best-practices/v/1.0.0"
+  timeouts {
+    create = "10m"
+  }
 }
 
 # ---------- PREVENTATIVE CONTROL: AWS Config ----------
